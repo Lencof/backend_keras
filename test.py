d4f895b75d14e92bd0c5223f53090f03793a3b30
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-07 06:14:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
-import unittest
+# test.py
+# __Author__ __Lencof__
+
+import unittest # use unittest
 from backend_keras import to_long_lang
 from backend_keras import process_sentence
 from backend_keras import load_obj
 from backend_keras import load_model
-import os
+import os # use os
 
 
+# create class TestBackendKeras(unittest.TestCase):
 class TestBackendKeras(unittest.TestCase):
 
+    # create def def test_model_loading(self):
     def test_model_loading(self):
         self.assertIsNotNone(load_model("/home/sovietspy2/PycharmProjects/backend_keras/model2.h5"))
 
+    # create def test_load_obj(self):
     def test_load_obj(self):
         with self.assertRaises(FileNotFoundError):
             load_obj('asd')
@@ -18,10 +24,12 @@ class TestBackendKeras(unittest.TestCase):
         self.assertEqual(os.path.exists('./int_to_languages.pkl'), True)
         self.assertIsNotNone(load_obj('int_to_languages'))
         self.assertIsNotNone(load_obj('vocab_to_int'))
-
+    
+    # create def def test_process_sentence(self):
     def test_process_sentence(self):
         self.assertEqual(process_sentence('Extreme , test with many ; stuff !!!'), 'Extreme test with many stuff'.lower())
-
+    
+    # create def test_to_long_lang(self):
     def test_to_long_lang(self):
         self.assertEqual(to_long_lang('en'), 'english')
         self.assertEqual(to_long_lang('hu'), 'hungarian')
@@ -31,4 +39,4 @@ class TestBackendKeras(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() 
